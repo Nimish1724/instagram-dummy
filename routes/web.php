ec82fb147bb105f8894b969e76d0ff15ef36c0a7
nimish <nimish@techmarbles.com>
2024-08-21 11:23:42 +0530

    react-added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/react-app/{any}', function () {
+    return view('app'); // Make sure to create a view for serving the React app
+})->where('any', '.*');
